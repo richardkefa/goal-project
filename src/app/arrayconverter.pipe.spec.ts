33aece9004b066fe0194f20090c3e0100aa61f48
richardkefa <richardkefa@gmail.com>
2020-08-26 11:42:30 +0300

    remove pipe

--- src/app/arrayconverter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArrayconverterPipe } from './arrayconverter.pipe';
-
-describe('ArrayconverterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArrayconverterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
